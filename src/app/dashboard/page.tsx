42d0611404e06c8bf14a459606b5af1ef21594f2
martongaspardy <Marton.Gaspardy@erstegroup.com>
2025-11-18 10:57:37 +0100

    Add withAuth for protected pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,13 @@
-export default function DashboardPage() {
-  return (
+import {withAuth} from "@/lib/withAuth";
+
+function DashboardPage() {
+    return (
     <div>
       <h2 className="text-2xl font-semibold">Dashboard</h2>
-      <p className="mt-4">Welcome to your SaaS app. Replace this with your feature modules.</p>
+      <p className="mt-4">Welcome SaaS app. Replace this with your feature modules.</p>
     </div>
   )
 }
+
+export default withAuth(DashboardPage);
+
