7a779596d2a0f3469f9ab3e6787001be42b9ede1
martongaspardy <Marton.Gaspardy@erstegroup.com>
2025-11-18 11:00:27 +0100

    Adjust welcome text

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ function DashboardPage() {
     return (
     <div>
       <h2 className="text-2xl font-semibold">Dashboard</h2>
-      <p className="mt-4">Welcome SaaS app. Replace this with your feature modules.</p>
+      <p className="mt-4">Welcome to your SaaS app. Replace this with your feature modules.</p>
     </div>
   )
 }
