a2afc16134b5d8fddbd1b230bd649bec3d448ad6
martongaspardy <Marton.Gaspardy@erstegroup.com>
2025-11-18 14:08:25 +0100

    Format code

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,15 +1,19 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { stripe } from '@/lib/stripe'
+import { NextRequest, NextResponse } from "next/server";
+import { stripe } from "@/lib/stripe";
 
 export const POST = async (req: NextRequest) => {
-  const buf = await req.arrayBuffer()
-  const body = Buffer.from(buf)
-  const sig = req.headers.get('stripe-signature') || ''
+  const buf = await req.arrayBuffer();
+  const body = Buffer.from(buf);
+  const sig = req.headers.get("stripe-signature") || "";
   try {
-    const event = stripe.webhooks.constructEvent(body, sig, process.env.STRIPE_WEBHOOK_SECRET || '')
+    const event = stripe.webhooks.constructEvent(
+      body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET || ""
+    );
     // handle events here
   } catch (error) {
-    return NextResponse.json({ error: 'Webhook error' }, { status: 400 })
+    return NextResponse.json({ error: "Webhook error" }, { status: 400 });
   }
-  return NextResponse.json({ received: true })
-}
+  return NextResponse.json({ received: true });
+};
